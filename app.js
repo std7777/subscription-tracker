bf900c1b6110c8e20cdc9458862e7c949580ac63
std7777 <priyanshiv8033@gmail.com>
2025-11-19 02:34:02 +0530

    Add environment variable configuration

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //creating express server
 import express from "express";
 
+import {PORT} from './config/env.js';
+
 const app = express();
 
 //create route -
@@ -10,8 +12,8 @@ app.get('/',(req,res)=>{
     res.send("Welcome to Subscription tracker API");
 });
 
-app.listen(3000,()=>{
-    console.log("Subscription tracker API is running on http://localhost:3000");
+app.listen(PORT,()=>{
+    console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
 })
 
 export default app;
---
+++ config/env.js
@@ -0,0 +1,6 @@
+import { config } from 'dotenv';
+//extract env variable from either the following files.(possible to switch between files)
+config({path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+//the variables can be exported from the file
+export const {PORT,NODE_ENV}=process.env;
