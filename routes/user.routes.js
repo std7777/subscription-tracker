a03c9c892abfab921e8d5d6bfabd8fb0b7951eb0
std7777 <priyanshiv8033@gmail.com>
2025-12-06 18:50:04 +0530

    add authorization for GET user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
+import authorize from '../middleware/auth.middleware.js';
+import {getUser, getUsers} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 //  /:id is a dynamic render.  dynamic id parameter
 
-userRouter.get('/',(req,res)=>res.send({title:'GET all users'}));
-userRouter.get('/:id',(req,res)=>res.send({title:'GET user details'}));
+userRouter.get('/',getUsers);
+userRouter.get('/:id',authorize, getUser);
 userRouter.post('/',(req,res)=>res.send({title:'CREATE new users'}));
 userRouter.put('/:id',(req,res)=>res.send({title:'UPDATE user'}));
 userRouter.delete('/:id',(req,res)=>res.send({title:'DELETE user'}));
